76c34a6d32ea2066f2e34a169528cf074b939d54
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-13 20:30:42 +0530

    Activity 1

---
+++ JS 30 day challenge/Day 1/Activity 1/task_1.js
@@ -0,0 +1,5 @@
+/*Declare a variable using VAR, assign it a number, 
+& log the value to the console*/
+
+var number = 12;
+console.log(number);
---
+++ JS 30 day challenge/Day 1/Activity 1/task_2.js
@@ -0,0 +1,5 @@
+/*Declare a variable using LET, assign it a string, 
+& log the value to the console*/
+
+let string = "Ayush";
+console.log(string);
