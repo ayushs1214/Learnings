b0e82e4bf9459e025698df3c46b2ad119eaefac0
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-13 19:59:14 +0530

    new tasks

--- JS 30 day challenge/variables_datatypes.js
+++ JS 30 day challenge/variables_datatypes.js
@@ -9,3 +9,33 @@ console.log(string);
 
 //ACTIVITY 2: Constant Declaration
 //TASK_3
+const isAvailable = true;
+console.log(isAvailable);
+
+//ACTIVITY 3: Data Types
+//TASK_4
+const myNumber = 42; // number
+const myString = "Hello, world!"; // string
+const myBoolean = true; // boolean
+const myObject = { name: "Alice", age: 30 }; // object
+const myArray = [1, 2, 3, 4, 5]; // array
+
+console.log(typeof myNumber);   // Output: number
+console.log(typeof myString);   // Output: string
+console.log(typeof myBoolean);  // Output: boolean
+console.log(typeof myObject);   // Output: object
+console.log(typeof myArray);    // Output: object
+
+//ACTIVITY 4: Reasigning Variables
+//TASK_5
+// Declare a variable using let and assign it an initial value
+let myVariable = "FIRST VALUE";
+
+// Log the initial value to the console
+console.log("Initial value:", myVariable);
+
+// Reassign a new value to the variable
+myVariable = "New Assigned";
+
+// Log the new value to the console
+console.log("Reassigned Value:", myVariable);
