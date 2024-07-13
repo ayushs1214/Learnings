d08ae33d802282b6ce3d27b5821de2868433ad4d
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-13 21:45:15 +0530

    Activity 4

---
+++ JS 30 day challenge/Day 1/Activity 4/task_5.js
@@ -0,0 +1,14 @@
+/*Declare a variable using LET, assign it an initial value, reassign
+it a new value, and then print the value of the variable to the console.*/
+
+// Declare a variable using let and assign it an initial value
+let myVariable = "FIRST VALUE";
+
+// Log the initial value to the console
+console.log("Initial value:", myVariable);
+
+// Reassign a new value to the variable
+myVariable = "New Assigned";
+
+// Log the new value to the console
+console.log("Reassigned Value:", myVariable);
