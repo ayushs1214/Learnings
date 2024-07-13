cf1585c0d63715aefd15044ed16f8fb5c8a1cd9c
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-13 21:31:26 +0530

    Activity 3

---
+++ JS 30 day challenge/Day 1/Activity 3/task_4.js
@@ -0,0 +1,14 @@
+/*Create a variable of different data types (Number, Boolean, strings,
+object, array) and log each variable's type using the TYPEOF Operator*/
+
+const myNumber = 42; // number
+const myString = "Hello, world!"; // string
+const myBoolean = true; // boolean
+const myObject = { name: "Alice", age: 30 }; // object
+const myArray = [1, 2, 3, 4, 5]; // array
+
+console.log(typeof myNumber);   // Output: number
+console.log(typeof myString);   // Output: string
+console.log(typeof myBoolean);  // Output: boolean
+console.log(typeof myObject);   // Output: object
+console.log(typeof myArray);    // Output: object
