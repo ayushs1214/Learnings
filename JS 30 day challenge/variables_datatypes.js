6c90e54306b063757f8b9a611f14edae9a1b5946
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-13 18:42:50 +0530

    JS CHALLENGE

---
+++ JS 30 day challenge/variables_datatypes.js
@@ -0,0 +1,11 @@
+//ACTIVITY 1: Variable Declaration
+//TASK_1
+var number = 12;
+console.log(number);
+
+//TASK_2
+let string = "Ayush";
+console.log(string);
+
+//ACTIVITY 2: Constant Declaration
+//TASK_3
