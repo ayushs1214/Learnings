594e2594690311fd6b2c6ddc2d5a883d7413f7a4
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-14 23:29:37 +0530

    Task 8

---
+++ JS 30 day challenge/Day 2/Activity 3/task_8.js
@@ -0,0 +1,18 @@
+/* Write a program to compare two numbers using
+< & > & log the result to the console. */
+
+function compareNumbers(a, b) {
+  if (a > b) {
+      console.log(`${a} is greater than ${b}`);
+  } else if (a < b) {
+      console.log(`${a} is less than ${b}`);
+  } else {
+      console.log(`${a} is equal to ${b}`);
+  }
+}
+
+// Example usage:
+let number1 = 10;
+let number2 = 20;
+
+compareNumbers(number1, number2);
