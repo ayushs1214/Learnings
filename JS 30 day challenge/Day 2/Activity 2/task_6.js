ae36d9c178c6b4df44f451f9543039186b3ee1e9
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-14 22:33:04 +0530

    task_6.js

---
+++ JS 30 day challenge/Day 2/Activity 2/task_6.js
@@ -0,0 +1,9 @@
+/* Use the += operator to add number to a
+variable and log the result to the console */
+
+let initialValue = 10;
+let numberToAdd = 5;
+
+initialValue += numberToAdd;
+
+console.log("The new value of the variable is:", initialValue);
