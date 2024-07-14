c4836afced4712d166cbbfedba86fc2aaf9a35a2
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-14 21:05:57 +0530

    Day 2

---
+++ JS 30 day challenge/Day 2/Activity 1/task_1.js
@@ -0,0 +1,12 @@
+/* Write a program to add two numbers and log the result to the console */
+
+// function addTwoNumbers(a, b) {
+//   var sum = a + b;
+//   console.log("The sum of a & b is:", sum);
+// }
+
+// // Using prompt to get user input (only works in browsers)
+// var a = parseInt(prompt("Enter the value of a:"));
+// var b = parseInt(prompt("Enter the value of b:"));
+
+// addTwoNumbers(a, b);
