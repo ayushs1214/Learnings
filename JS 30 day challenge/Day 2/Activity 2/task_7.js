e3d89f83446914cfda0bb5b9d8cb358295d77363
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-14 23:03:29 +0530

    task_7

---
+++ JS 30 day challenge/Day 2/Activity 2/task_7.js
@@ -0,0 +1,7 @@
+/* Use the -= operator to add number to a
+variable and log the result to the console */
+
+let initialValue = 15;
+let newValue = 5
+initialValue -= newValue;
+console.log("The new value of the variable is:", initialValue);
