9b6513dee034bc51b793c54518534943f49efb6b
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-15 23:10:37 +0530

    Activity 2

--- JS 30 day challenge/Day 3/Activity 2/task_3.js
+++ JS 30 day challenge/Day 3/Activity 2/task_3.js
@@ -1,23 +1,24 @@
 /* Write a program to find the largest of three
 numbers using nested if-else statments */
 
-function largestNumber (a,b)
-{
-  if(a>b)
-  {
-    console.log (a, "is bigger than", b);
-  }
-  else if (a<b)
-  {
-    console.log (b, "is bigger than", a);
-  }
-  else
-  {
-    console.log ("Both ", a, "&", b, "are equal");
+function largestNumber(a, b, c) {
+  if (a >= b) {
+      if (a >= c) {
+          console.log(a + " is the largest number.");
+      } else {
+          console.log(c + " is the largest number.");
+      }
+  } else {
+      if (b >= c) {
+          console.log(b + " is the largest number.");
+      } else {
+          console.log(c + " is the largest number.");
+      }
   }
 }
 
-//Exmaple number
-largestNumber(10,20);
-largestNumber(30,15);
-largestNumber(20,20);
+// Example usage:
+largestNumber(10, 20, 30); // 30 is the largest number.
+largestNumber(30, 15, 10); // 30 is the largest number.
+largestNumber(20, 20, 10); // 20 is the largest number.
+largestNumber(10, 30, 30); // 30 is the largest number.
