068b97f85fc9167590620750e4b8942827f9833d
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-17 17:46:39 +0530

    Day 4

---
+++ JS 30 day challenge/Day 4/Activity 1/task_2.js
@@ -0,0 +1,8 @@
+/* Write a program to print the multiplication table of 5 using a for loop. */
+
+const number = 5;
+
+for (let i = 1; i <= 10; i++)
+  {
+  console.log(`${number} x ${i} = ${number * i}`);
+  }
---
+++ JS 30 day challenge/Day 4/Activity 2/task_3.js
@@ -0,0 +1,3 @@
+/* Write a program to calculate the sum of numbers from 1 to 10 using a while loop */
+
+
