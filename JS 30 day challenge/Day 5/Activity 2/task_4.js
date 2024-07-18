1295d5a223b650ab8d96d1e63357fdb68e4df2bc
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-18 23:44:21 +0530

    task 4

---
+++ JS 30 day challenge/Day 5/Activity 2/task_4.js
@@ -0,0 +1,9 @@
+/* Write a function expression to concatenate two strings & return the result */
+
+const concatenateStrings = function(str1, str2) {
+  return str1 + str2;
+};
+
+// Example usage
+let result = concatenateStrings("Hello, ", "World!");
+console.log(result); // Output: Hello, World!
