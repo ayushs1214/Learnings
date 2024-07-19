ef9a048bdb5831a2af4c922a3ba4128ffe70f42c
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-19 22:08:31 +0530

    Name & age

---
+++ JS 30 day challenge/Day 5/Activity 4/task_8.js
@@ -0,0 +1,10 @@
+/* Write a function that takes a persons name, age, & returns a greeting message */
+
+function greetName(str, int)
+{
+  console.log("Hello " + str + ", you are " + int + " years old.");
+}
+
+//Example
+greetName("John", 30);
+greetName("Ayush", 18);
