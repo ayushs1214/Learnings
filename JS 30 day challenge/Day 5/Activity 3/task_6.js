5961fd00a671509ceb22a9c8b2cf632c7ba96664
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-19 13:54:59 +0530

    Strin & Char

--- JS 30 day challenge/Day 5/Activity 3/task_6.js
+++ JS 30 day challenge/Day 5/Activity 3/task_6.js
@@ -1,1 +1,10 @@
 /* Write an arrow function to check if a string contains a specific character & return a boolean value */
+
+const containsCharacter = (str, char) => str.includes(char);
+
+// Example usage
+let result1 = containsCharacter("Hello, World!", "W");
+console.log(result1); // Output: true
+
+let result2 = containsCharacter("Hello, World!", "z");
+console.log(result2); // Output: false
