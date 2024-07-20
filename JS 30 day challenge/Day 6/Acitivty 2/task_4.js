eab0c41f6676356a1c5c0f9092a70279c37bafa3
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-21 04:15:52 +0530

    pop method

--- JS 30 day challenge/Day 6/Acitivty 2/task_4.js
+++ JS 30 day challenge/Day 6/Acitivty 2/task_4.js
@@ -1,1 +1,9 @@
 /* Use the 'pop' method to remove the last element from the array & log the updated array. */
+
+const array = [1, 2, 3, 4, 5];
+
+// Use the 'pop' method to remove the last element
+array.pop();
+
+// Log the updated array
+console.log(array);
