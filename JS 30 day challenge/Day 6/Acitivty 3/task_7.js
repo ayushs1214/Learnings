d388b1a089bd81c8acf64706b99908e3004f96e1
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-21 04:23:22 +0530

    MAP

---
+++ JS 30 day challenge/Day 6/Acitivty 3/task_7.js
@@ -0,0 +1,9 @@
+/* Use the 'map' method to create a new array where each number is doubled & log the new array */
+
+const array = [1, 2, 3, 4, 5];
+
+// Use the 'map' method to create a new array where each number is doubled
+const doubledArray = array.map(number => number * 2);
+
+// Log the new array
+console.log(doubledArray);
