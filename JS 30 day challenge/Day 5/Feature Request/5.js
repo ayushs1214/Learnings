846eb8da2a0e361db309bb50d4eb2e0ccdf7b028
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-20 23:23:59 +0530

    Feature 5

--- JS 30 day challenge/Day 5/Feature Request/5.js
+++ JS 30 day challenge/Day 5/Feature Request/5.js
@@ -1,2 +1,20 @@
 /* Higher Order Function: Write a script that includes a higher-order-function to apply a given function multiple times. */
 
+function higherOrderFunction(fn, times)
+{
+  return function()
+  {
+    for (var i = 0; i < times; i++)
+    {
+      fn();
+    }
+  };
+}
+
+//Example
+var hello = function()
+{
+  console.log("Hello World!");
+};
+
+higherOrderFunction(sum, 10);
