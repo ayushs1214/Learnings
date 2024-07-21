231705321a8e7b4ec03b68f25f1ce23e793658dd
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-21 15:06:30 +0530

    Matrix

---
+++ JS 30 day challenge/Day 6/Acitivty 5/task_12.js
@@ -0,0 +1,11 @@
+/* Create a two-dimensional array [matrix] & log the entire array to the console. */
+
+// Create a 3x3 matrix
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+// Log the entire matrix to the console
+console.log(matrix);
