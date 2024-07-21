7faf4c95c35d813f332a978e994fb211e9625ed8
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-21 17:03:07 +0530

    Object 1

---
+++ JS 30 day challenge/Day 7/Activity 1/task_1.js
@@ -0,0 +1,10 @@
+/* Create an object representing a book with properties like
+title, author, & year & log the object to the console. */
+
+const object = {
+  title: "The Hobbit",
+  author: "J.R.R. Tolkien",
+  year: 1937,
+};
+
+console.log(object);
