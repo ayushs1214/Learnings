d01830270dff66bd9e9f3c0559d078d72f4f5972
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-21 15:35:09 +0530

    Array Transformation

--- JS 30 day challenge/Day 6/Feature Request/2.js
+++ JS 30 day challenge/Day 6/Feature Request/2.js
@@ -1,3 +1,20 @@
 /* Array Transformation Script: Create a script that uses 'map',
 'filter', & 'reduce' methods to transform & agregate array data */
 
+// Initial arrays
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+
+// Using 'map' to create a new array where each element is doubled
+const mappedArray = array1.map(num => num * 2); // [2, 4, 6]
+
+// Using 'filter' to create a new array with only even numbers
+const filteredArray = array2.filter(num => num % 2 === 0); // [4, 6]
+
+// Using 'reduce' to calculate the sum of all numbers in the array
+const reducedSum = array1.reduce((acc, num) => acc + num, 0); // 6
+
+// Logging the results
+console.log("Mapped Array:", mappedArray);
+console.log("Filtered Array:", filteredArray);
+console.log("Reduced Sum:", reducedSum);
