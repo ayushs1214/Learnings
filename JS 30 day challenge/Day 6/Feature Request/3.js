e63bf6175f0b409cb6b3015dffe792af34bedc43
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-21 15:41:24 +0530

    For & ForEach Method

--- JS 30 day challenge/Day 6/Feature Request/3.js
+++ JS 30 day challenge/Day 6/Feature Request/3.js
@@ -1,3 +1,18 @@
 /* Array Iteration Script: Write a script that iterates over an array
 using both 'for' loop and 'forEach' method and logs each element */
 
+const array = [1,2,3,4,5];
+console.log("Using for loop:");
+
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+  console.log(element);
+}
+
+console.log("Array forEach element");
+
+  array.forEach(element =>
+  {
+    console.log(element);
+  }
+);
