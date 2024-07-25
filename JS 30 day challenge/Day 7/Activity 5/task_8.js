31bac0ff518589131feb627b20d1b3883724e863
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-25 12:38:00 +0530

    Object.keys & Values

--- JS 30 day challenge/Day 7/Activity 5/task_8.js
+++ JS 30 day challenge/Day 7/Activity 5/task_8.js
@@ -1,1 +1,13 @@
 /* Use a 'for...in' loop to iterate over the properties of the book object & log each property & its value */
+
+const book = {
+  title: "The Great Gatsby",
+  author: "F. Scott Fitzgerald",
+  published: 1925
+};
+
+for (let property in book) {
+  if (book.hasOwnProperty(property)) {
+      console.log(`${property}: ${book[property]}`);
+  }
+}
--- JS 30 day challenge/Day 7/Activity 5/task_9.js
+++ JS 30 day challenge/Day 7/Activity 5/task_9.js
@@ -1,1 +1,23 @@
-/* Use 'obect.keys' and 'object.values' methods to log all the keys & values of the book object. */
+/* Use 'object.keys' and 'object.values' methods to log all the keys & values of the book object. */
+
+const book = {
+  1: {
+    id: 1,
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    year: 1925,
+  },
+  
+  2: {
+    id: 2,
+    title: 'The Catcher in the Rye',
+    author: 'J.D. Salinger',
+    year: 1951,
+  }
+}
+
+const key = Object.keys(book);
+const value = Object.values(book);
+
+console.log("key:",key);
+console.log("value:",value);
