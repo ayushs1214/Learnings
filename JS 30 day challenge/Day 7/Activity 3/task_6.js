f66834b2d23b77e59d1d1c3158dd944fabfc7d78
Your Name <106509968+ayushs1214@users.noreply.github.com>
2024-07-27 23:06:48 +0530

    Object.keys/Values

--- JS 30 day challenge/Day 7/Activity 3/task_6.js
+++ JS 30 day challenge/Day 7/Activity 3/task_6.js
@@ -1,2 +1,16 @@
 /* Access & log the name of the library & the titles of all the books in the library. */
 
+// Create a library object
+const library = {
+  name: "My Library",
+  books: ["Book 1", "Book 2", "Book 3"]
+};
+
+// Access and log the name of the library
+console.log("Library Name:", library.name);
+
+// Access and log the titles of all the books in the library
+console.log("Book Titles:");
+library.books.forEach(title => {
+  console.log(title);
+});
---
+++ JS 30 day challenge/Day 7/Feature Request/3.js
@@ -0,0 +1,28 @@
+/* Object Iteration Script: Write a script that demonstrates iterating over 
+an object's properties using 'for..in' loop & 'object.keys/object.values' */
+
+const person = {
+  name: 'Alice',
+  age: 30,
+  profession: 'Engineer'
+};
+
+// Using for..in loop
+console.log('Using for..in loop:');
+for (let key in person) {
+  if (person.hasOwnProperty(key)) {
+      console.log(`${key}: ${person[key]}`);
+  }
+}
+
+// Using Object.keys
+console.log('Using Object.keys:');
+Object.keys(person).forEach(key => {
+  console.log(`${key}: ${person[key]}`);
+});
+
+// Using Object.values
+console.log('Using Object.values:');
+Object.values(person).forEach(value => {
+  console.log(value);
+});
